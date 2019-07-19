32cc6f2782e7c7d2bcce6d0d0c8118dab3ec0d69
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2019-07-20 00:23:36 +0100

    Add files via upload

---
+++ fund_wallet.php
@@ -0,0 +1,102 @@
+<html>
+<head>
+<title>Ezekoconcept | ACCOUNT DETAILS</title>
+   
+</head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+<body>
+
+	
+	<div class="w3-container" >
+    <h1 class="w3-xxxlarge w3-text-red"><b>Account details</b></h1>
+        <a href="dashboard.php"><button class="w3-hover-green" style="background-color: #0088ff">DASHBOARD</button></a>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+
+
+	<div class="w3-container">
+
+
+<p>ACCOUNT NAME: <span class='w3-text-blue'><b> ADEJOBI EZEKIEL ADEWOLE</b></span></p>
+<p class= "w3-panel-danger"><li> Contact us with the details of your payment After payment has been made</li></p>
+
+  <table class="w3-table-all w3-hoverable">
+    <thead>
+      <tr class="w3-red">
+      <th>NO</th>
+        <th>BANK</th>
+        <th>DETAILS</th>
+      </tr>
+    </thead>
+    <tr class="w3-hover-blue">
+      <td>1</td>
+      <td>FIRST BANK</td>
+      <td>3063857057</td>
+    </tr>
+    <tr class="w3-hover-red">
+      <td>2</td>
+      <td>UBA</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-green">
+      <td>3</td>
+      <td>ZENITH</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-pink">
+      <td>4</td>
+      <td>GTB</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-blue">
+      <td>5</td>
+      <td>ACCESS </td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-yellow">
+      <td>6</td>
+      <td>DIAMOND</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-purple">
+      <td>7</td>
+      <td>WEMA</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-blue">
+      <td>8</td>
+      <td>-</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-blue">
+      <td>9</td>
+      <td>-</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-blue">
+      <td>10</td>
+      <td>-</td>
+       <td></td>
+    </tr>
+	<tr class="w3-hover-blue">
+      <td>11</td>
+      <td></td>
+       <td></td>
+    </tr>
+	  </table>
+
+
+</div></div>
+<br/>
+
+		
+
+
+<br><br>
+
+
+	
+</body>
+</html>
